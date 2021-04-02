194e52bbb22de4ddc14ec051d0467608a13da18b
BobGame <liulingbo@foxmail.com>
2021-04-02 18:07:35 +0800

    update home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-  { path: '', redirectTo: 'slide', pathMatch: 'full' },
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '', redirectTo: 'slide', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeComponentModule) },
   { path: 'sudo', loadChildren: () => import('./game/sudoku/sd-app.module').then(m => m.SdAppComponentModule) },
   { path: 'guess', loadChildren: () => import('./game/guess/gs-app.module').then(m => m.GsAppComponentModule) },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,41 @@
 import { Component, OnInit } from '@angular/core'
 import { AllService } from '../common/services/all.service'
 
+interface AllGameItem {
+  textKey: string
+  routerLink: string
+  image: string
+  color: string
+}
+
 @Component({
   selector: 'nw-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+
 export class HomeComponent implements OnInit {
+
+  AllGames: AllGameItem[] = [
+    {
+      textKey: 'sudoku.name',
+      routerLink: '/sudo',
+      image: 'assets/images/img-sudoku.png',
+      color: '',
+    },
+    {
+      textKey: 'guess.name',
+      routerLink: '/guess',
+      image: 'assets/images/img-sudoku.png',
+      color: '',
+    },
+    {
+      textKey: 'slide.name',
+      routerLink: '/slide',
+      image: 'assets/images/img-sudoku.png',
+      color: '',
+    },
+  ]
 
   constructor(
     private all: AllService
