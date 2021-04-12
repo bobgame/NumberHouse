4adb1ae359f5b4d4b9959c330b1c6bde9490de04
BobGame <liulingbo@foxmail.com>
2021-04-12 16:13:55 +0800

    update slide animation

--- src/app/game/slide/services/slide-data.service.ts
+++ src/app/game/slide/services/slide-data.service.ts
@@ -40,6 +40,9 @@ export class SlideDataService {
     [1, 2, 3, 4, 5, 6, 7],
     [1, 2, 3, 4, 5, 6, 7, 8],
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   ]
 
   isSwiping = false
@@ -300,7 +303,7 @@ export class SlideDataService {
         this.slideData.slideTimes = 0
         this.gameWin()
       }
-    }, 800)
+    }, 300)
   }
 
   fillEmptyPoses(emptyPoses: SlidePos[], plusPos: SlidePos) {
